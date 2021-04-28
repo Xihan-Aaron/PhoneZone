4cc0ce3636a86e627bd2128576a9b59707351b16
Liam Ireland <lire2640@uni.sydney.edu.au>
2021-04-28 16:57:42 +1000

    main page item state using helper function

--- controllers/main.js
+++ controllers/main.js
@@ -2,6 +2,7 @@ var express = require('express');
 var crypto = require('crypto');
 const User = require('../models/users');
 const PhoneListing = require('../models/phoneListing');
+const helper = require('./helper');
 
 
 module.exports.main = async function(req,res,next){
@@ -34,13 +35,14 @@ module.exports.search = async function(req,res,next){
 module.exports.selectItem = async function(req,res,next){
 	try{
 		item_id = req.body.selectItem
-		console.log('selectItem', req.body.item_id)
-		item = await PhoneListing.getItemById(item_id)
-		seller = await User.getUserById(item.seller)
+		console.log('selectItem', item_id)
+		items = await PhoneListing.getItemById(item_id)
+		item = (await helper.extractNames([items]))[0]
+		console.log("item", item);
 		// fullname = seller.firstname + seller.lastname
+		res.render('main.ejs',{user_id:req.session.user_id, item:item})
 	}catch(err){
 		err.statusCode=500
 		next(err)
 	}
-	res.render('main.ejs',{user_id:req.session.user_id, item:item})
 }
